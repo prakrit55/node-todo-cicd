93e77c076b0d128398b5cc6c91bcd65448866f22
Prakriti Mandal <98270250+prakrit55@users.noreply.github.com>
2023-04-04 21:04:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express'),
-    bodyParser = require('body-parser'),
+    bodyParser = requirel('body-parser'),
     // In order to use PUT HTTP verb to edit item
     methodOverride = require('method-override'),
     // Mitigate XSS using sanitizer
